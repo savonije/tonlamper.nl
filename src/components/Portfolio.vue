d3766870bd571dbe59671496c7cb5e0b44edc818
Kevin Savonije <kevin@23g.nl>
2024-07-01 21:57:13 +0200

    feat: portfolio

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import artworks from './kunstwerken.json'
+</script>
+
+<template>
+	<div class="columns-3 gap-3">
+		<figure v-for="artwork in artworks">
+			<img
+				:src="artwork.image"
+				:alt="artwork.name"
+				class="mb-3 h-auto w-full max-w-full rounded-lg"
+				loading="lazy"
+			/>
+			<figcation>{{ artwork.name }}</figcation>
+		</figure>
+	</div>
+</template>
