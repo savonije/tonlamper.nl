6f152d52117b43640e5d5230c0af09777022e3a9
Kevin Savonije <kevin@23g.nl>
2024-08-13 20:14:23 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
-});
+	site: 'https://www.tonlamper.nl',
+	integrations: [vue(), tailwind(), sitemap()]
+})
