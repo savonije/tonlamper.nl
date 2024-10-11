929c8c007a26e3b3f9dce4997a74ca0bba3e8a8e
Kevin Savonije <kevin@23g.nl>
2024-10-11 12:23:31 +0200

    fix: portfolio order should be reversed and not random

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,29 +1,13 @@
 <script lang="ts" setup>
 import artworks from '@components/artworks.json'
-import type { Artworks } from '../types'
 
-function shuffleObject(obj: Artworks) {
-	const entries = Object.entries(obj)
-
-	for (let i = entries.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1))
-		;[entries[i], entries[j]] = [entries[j], entries[i]]
-	}
-
-	const shuffledObj: Artworks = {}
-	entries.forEach(([key, value], index) => {
-		shuffledObj[index + 1] = value
-	})
-
-	return shuffledObj
-}
-
-const shuffledArtworks: Artworks = shuffleObject(artworks)
+// Reverse the artworks array
+const reversedArtworks = Object.values(artworks).reverse()
 </script>
 
 <template>
 	<div class="columns-2 gap-3 lg:columns-3">
-		<figure v-for="artwork in shuffledArtworks">
+		<figure v-for="artwork in reversedArtworks">
 			<a :href="'/portfolio/' + artwork.slug">
 				<img
 					:src="artwork.images[0]"
