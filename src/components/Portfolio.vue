8d0102c5fe0284b8c358d5aee6637d9b8f1c5005
Kevin Savonije <kevin@23g.nl>
2024-10-14 09:10:06 +0200

    feat: add filters to portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,13 +1,48 @@
 <script lang="ts" setup>
+import { ref, computed } from 'vue'
 import artworks from '@components/artworks.json'
 
-// Reverse the artworks array
+const categories = ['ets', 'lino', 'ruimtelijk']
+
 const reversedArtworks = Object.values(artworks).reverse()
+
+const selectedCategory = ref<string | null>(null)
+
+const filteredArtworks = computed(() => {
+	if (!selectedCategory.value) {
+		return reversedArtworks
+	}
+
+	return reversedArtworks.filter(
+		(artwork) =>
+			selectedCategory.value !== null && artwork.category.includes(selectedCategory.value)
+	)
+})
+
+const selectCategory = (category: string) => {
+	if (selectedCategory.value === category) {
+		return (selectedCategory.value = null)
+	}
+
+	selectedCategory.value = category
+}
 </script>
 
 <template>
-	<div class="columns-2 gap-3 lg:columns-3">
-		<figure v-for="artwork in reversedArtworks">
+	<div class="mb-3 flex gap-6">
+		<button
+			type="button"
+			v-for="category in categories"
+			:key="category"
+			@click="selectCategory(category)"
+			:class="{ 'font-bold': selectedCategory === category }"
+		>
+			{{ category }}
+		</button>
+	</div>
+
+	<div class="columns-1 gap-3 sm:columns-2 lg:columns-3">
+		<figure v-for="artwork in filteredArtworks">
 			<a :href="'/portfolio/' + artwork.slug">
 				<img
 					:src="artwork.images[0]"
@@ -23,7 +58,15 @@ const reversedArtworks = Object.values(artworks).reverse()
 	</div>
 </template>
 
-<style>
+<style scoped>
+button {
+	@apply text-black;
+
+	&:hover {
+		@apply underline;
+	}
+}
+
 figure {
 	@apply relative;
 
