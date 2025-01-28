15f95aa1bae68a78b57e1d3ed8cab81081b53d5d
Kevin Savonije <kevin@23g.nl>
2025-01-28 08:23:55 +0100

    chore: update tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
-import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.tonlamper.nl',
-	integrations: [vue(), tailwind(), sitemap()]
+	integrations: [vue(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()]
+	}
 })
